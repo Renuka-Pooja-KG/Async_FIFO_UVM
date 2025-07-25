a63c1691e6b795b17ec495c43b11aa6be171534c
Renuka-Pooja-KG <renuka.pooja@pravegasemi.com>
2025-07-25 15:54:55 +0530

    Interface added to compile_list and removed from package

--- Verification/SIM/compile_list.f
+++ Verification/SIM/compile_list.f
@@ -5,5 +5,9 @@
 /home/sgeuser100/renuka_dv/Async_FIFO_UVM/RTL/wptr_full.sv
 /home/sgeuser100/renuka_dv/Async_FIFO_UVM/RTL/wr_ndff_synch.sv
 
+/home/sgeuser100/renuka_dv/Async_FIFO_UVM/Verification/UVME/top/interface/wr_interface.sv
+/home/sgeuser100/renuka_dv/Async_FIFO_UVM/Verification/UVME/top/interface/rd_interface.sv
 /home/sgeuser100/renuka_dv/Async_FIFO_UVM/Verification/UVME/package/verification_pkg.sv
 /home/sgeuser100/renuka_dv/Async_FIFO_UVM/Verification/UVME/top/async_fifo_top_tb.sv
+
+
--- Verification/UVME/package/verification_pkg.sv
+++ Verification/UVME/package/verification_pkg.sv
@@ -4,35 +4,35 @@ import uvm_pkg::*;
 `include "uvm_macros.svh"
 
 // Include all the files in the package
-`include "../top/interface/wr_interface.sv"
-`include "../top/interface/rd_interface.sv"
+//`include "./../UVME/top/interface/wr_interface.sv"
+//`include "./../UVME/top/interface/rd_interface.sv"
 
-`include "../sequence/write_sequence_item.sv"
-`include "../sequence/read_sequence_item.sv"
+`include "./../UVME/sequence/write_sequence_item.sv"
+`include "./../UVME/sequence/read_sequence_item.sv"
 
-`include "../write_agent/write_driver.sv"
-`include "../read_agent/read_driver.sv"
+`include "./../UVME/write_agent/write_driver.sv"
+`include "./../UVME/read_agent/read_driver.sv"
 
-`include "../write_agent/write_sequencer.sv"
-`include "../read_agent/read_sequencer.sv"
+`include "./../UVME/write_agent/write_sequencer.sv"
+`include "./../UVME/read_agent/read_sequencer.sv"
 
-`include "../write_agent/write_monitor.sv"
-`include "../read_agent/read_monitor.sv"
+`include "./../UVME/write_agent/write_monitor.sv"
+`include "./../UVME/read_agent/read_monitor.sv"
 
-`include "../env/scoreboard.sv"
-`include "../env/write_coverage.sv"
-`include "../env/read_coverage.sv"
+`include "./../UVME/env/scoreboard.sv"
+`include "./../UVME/env/write_coverage.sv"
+`include "./../UVME/env/read_coverage.sv"
 
-`include "../env/env.sv"
+`include "./../UVME/env/env.sv"
 
-`include "../sequence/write_base_sequence.sv"
-`include "../sequence/read_base_sequence.sv"
-`include "../sequence/write_until_wfull_seq.sv"
-`include "../sequence/read_until_rdempty_seq.sv"
+`include "./../UVME/sequence/write_base_sequence.sv"
+`include "./../UVME/sequence/read_base_sequence.sv"
+`include "./../UVME/sequence/write_until_wfull_seq.sv"
+`include "./../UVME/sequence/read_until_rdempty_seq.sv"
 
-`include "../test/base_test.sv"
-`include "../test/write_until_wfull_test.sv"
-`include "../test/read_until_rdempty_test.sv"
+`include "./../UVME/test/base_test.sv"
+`include "./../UVME/test/write_until_wfull_test.sv"
+`include "./../UVME/test/read_until_rdempty_test.sv"
 
 
 endpackage
