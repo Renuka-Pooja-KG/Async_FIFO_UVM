22479d1c56c8d3b254d2a0c1f61c41911626f235
Renuka-Pooja-KG <renuka.pooja@pravegasemi.com>
2025-07-28 16:13:02 +0530

    As suggested by AI, interdace added to write and read agent

--- Verification/UVME/read_agent/read_agent.sv
+++ Verification/UVME/read_agent/read_agent.sv
@@ -5,6 +5,7 @@ class read_agent extends uvm_agent;
   read_monitor   m_monitor;
   read_sequencer m_sequencer;
 
+  virtual rd_interface rd_vif;
   //uvm_analysis_port #(read_sequence_item) rd_agent_analysis_port;
 
   function new(string name = "read_agent", uvm_component parent = null);
@@ -19,6 +20,17 @@ class read_agent extends uvm_agent;
       m_driver    = read_driver::type_id::create("m_driver", this);
       m_sequencer = read_sequencer::type_id::create("m_sequencer", this);
     end
+
+    // Get the virtual interface from config_db and assign to driver and monitor
+ 
+    if (!uvm_config_db#(virtual rd_interface)::get(this, "", "rd_vif", rd_vif)) begin
+      `uvm_fatal("NOVIF", "Could not get rd_vif from uvm_config_db")
+    end
+    if (is_active == UVM_ACTIVE) begin
+      m_driver.rd_vif = rd_vif;
+    end
+    m_monitor.rd_vif = rd_vif;
+
     `uvm_info(get_type_name(), "read_agent build_phase completed", UVM_LOW)
   endfunction
 
@@ -30,4 +42,4 @@ class read_agent extends uvm_agent;
     end
     `uvm_info(get_type_name(), "read_agent connect_phase completed", UVM_LOW)
   endfunction
-endclass 
+endclass
--- Verification/UVME/write_agent/write_agent.sv
+++ Verification/UVME/write_agent/write_agent.sv
@@ -5,6 +5,8 @@ class write_agent extends uvm_agent;
   write_monitor   m_monitor;
   write_sequencer m_sequencer;
 
+  virtual wr_interface wr_vif;
+
   //uvm_analysis_port #(write_sequence_item) wr_agent_analysis_port;
 
   function new(string name = "write_agent", uvm_component parent = null);
@@ -19,6 +21,16 @@ class write_agent extends uvm_agent;
       m_driver    = write_driver::type_id::create("m_driver", this);
       m_sequencer = write_sequencer::type_id::create("m_sequencer", this);
     end
+
+    // Get the virtual interface from config_db and assign to driver and monitor
+    if (!uvm_config_db#(virtual wr_interface)::get(this, "", "wr_vif", wr_vif)) begin
+      `uvm_fatal("NOVIF", "Could not get wr_vif from uvm_config_db")
+    end
+    if (is_active == UVM_ACTIVE) begin
+      m_driver.wr_vif = wr_vif;
+    end
+    m_monitor.wr_vif = wr_vif;
+
     `uvm_info(get_type_name(), "write_agent build_phase completed", UVM_LOW)
   endfunction
 
@@ -33,4 +45,4 @@ class write_agent extends uvm_agent;
     end
     `uvm_info(get_type_name(), "write_agent connect_phase completed", UVM_LOW)
   endfunction
-endclass 
+endclass
